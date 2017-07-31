1f4b27aeb0dedeb5c5464a009a224aacad9775cf
Jakub Stańczak <kuba0095@gmail.com>
2017-07-31 19:26:08 +0200

    Create first try model

--- retina_scan/settings.py
+++ retina_scan/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data_module',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_root')
