28c3d8dc92aad396e50751bc7f95cd6681f0ead4
Jakub Stańczak <kuba0095@gmail.com>
2017-10-03 21:01:45 +0200

    Use new generator

--- neural_network/nn_manager/DataGenerator.py
+++ neural_network/nn_manager/DataGenerator.py
@@ -1,9 +1,16 @@
+import os
+
+import numpy as np
+from PIL.Image import Image
+
 from neural_network.nn_manager.AbstractDataGenerator import AbstractDataGenerator
 
 
 class DataGenerator(AbstractDataGenerator):
     def flow_from_directory(self, path):
-        pass
+        for name in os.listdir(path):
+            if name.endswith('.jpg'):
+                yield name, np.asarray(Image.open(os.path.join(path, name)))
 
     def flow(self, files, names):
         for file, name in zip(files, names):
